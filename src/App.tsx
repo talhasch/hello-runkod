8f8e4d35591bcad95ae99b1f6f0db9890223f0ff
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-08-02 21:05:18 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://runkod.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Runkod
         </a>
       </header>
     </div>
